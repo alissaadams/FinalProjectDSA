0bba276590e874c54b24f0ef0672d26a6b832050
tongshan77 <tongshan29@gmail.com>
2024-11-29 22:07:36 -0500

    Update Trie Part

--- hashAndTrie.py
+++ hashAndTrie.py
@@ -3,6 +3,77 @@ import json
 class HashMap:
     hashMap = {}
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.node_info = []
+        self.word_completed = False
+
+    def end_word(self):
+        self.word_completed = True
+
+    def not_end_word(self):
+        self.word_completed = False
 
 class Trie:
-    trie = 0 # placeholder
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, title, title_info):
+        node = self.root
+
+        # traverse each character in the title
+        for char in title:
+            # check if the character exists in the current node's children
+            if char in node.children:
+                node = node.children[char]
+            # if not exits, create a new child node for this character
+            else:
+                node.children[char] = TrieNode()
+
+        # mark the current node as the end of the word to indicate this path has a complete word
+        node.end_word()
+        # add the information of the title
+        node.node_info.append(title_info)
+
+    def search(self, title):
+        node = self.root
+
+        # traverse each character in the title string
+        for char in title:
+            if char in node.children:
+                node = node.children[char]
+
+            # if the character not exits in the node of children indicates the title not exits
+            else:
+                return []
+
+        # check if the current node is the last character of a completed word
+        if node.word_completed:
+            return node.node_info
+        else:
+            return []
+
+
+    def startswith(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                # Prefix not found
+                return []
+        # The prefix path exists, gather all relevant title information starting from this node
+        return self.all_title_info(node)
+
+    def all_title_info(self, node):
+        # use to store all title information
+        results = []
+        if node.word_completed:
+            results.extend(node.node_info)
+
+        # traversal all child nodes of the current node
+        for childnode in node.children.values():
+            results.extend(self.all_title_info(childnode))
+        # Return the results list containing all title information
+        return results
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
-import hashAndTrie
+from hashAndTrie import Trie
 
+# initialize the Trie
+trie = Trie()
 
 dataFile = 'data/meta_Appliances.jsonl'
 
@@ -13,8 +15,28 @@ with open(dataFile, 'r') as file: # help with loading items from https://amazon-
             features = item.get('details', None)
 
             if title: # if title exists, append to the data array
-                    data.append({'title': title,
-                                 'features': features if features else "No features information"})
+                info_item = {'title': title,
+                             'features': features if features else "No features information"
+                             }
+                data.append(info_item)
+                trie.insert(title.lower(), info_item)
 
 print(data[0]) # shows data format when put in array (just for example)
 print(data[4])
+print(data[8])
+
+
+# test for the Trie
+title = "ROVSUN Ice Maker Machine Countertop"
+results_title = trie.search(title.lower())
+print(f"Search results for title '{title}':", results_title, end='')
+
+prefix = "Whirlpool"
+results_prefix = trie.startswith(prefix.lower())
+print(f"Search results for prefix '{prefix}':", results_prefix, end='')
+
+nonetitle = "none"
+results_nonetitle = trie.search(nonetitle.lower())
+print(f"Search results for title '{nonetitle}':", results_nonetitle)
+
+print(f"Total items loaded: {len(data)}", end='')
